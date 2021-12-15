3a40d3a666e2dfe338570b49d6c1f676bcbf3510
Dylan Phelan <dtphelan1@gmail.com>
2021-12-14 21:32:53 -0500

    improvements to handle more lazy loading logic and more generic page handling

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const wholeGrid = document.getElementById('merch-grid');
-const newGrid = document.createDocumentFragment();
-const inStock = [];
-const soldOut = [];
+const wholeGrid = document.getElementById("merch-grid");
+if (wholeGrid) {
+  const newGrid = document.createDocumentFragment();
+  const inStock = [];
+  const soldOut = [];
 
-Array.from(wholeGrid.children).forEach((li) => {
-  const cloneLi = li.cloneNode(true);
-  if (li.querySelector('p.price.sold-out')) {
-    soldOut.push(cloneLi);
-  } else {
-    inStock.push(cloneLi);
-  }
-});
+  Array.from(wholeGrid.children).forEach((li) => {
+    const cloneLi = li.cloneNode(true);
+    if (li.querySelector("p.price.sold-out")) {
+      soldOut.push(cloneLi);
+    } else {
+      inStock.push(cloneLi);
+    }
+  });
 
-soldOut.forEach((li) => {
-    li.style="background:rgba(210,210,210,0.7)"
-})
+  // Add a light faded look to all sold out items
+  soldOut.forEach((li) => {
+    li.style = "opacity: 0.7";
+  });
 
-newGrid.append(...inStock);
-newGrid.append(...soldOut);
+  newGrid.append(...inStock);
+  newGrid.append(...soldOut);
 
-// Work around lazy loading of BC photos
-Array.from(newGrid.querySelectorAll('img')).forEach((img) => {
-  if (img.dataset.original) { 
-      img.src = img.dataset.original; 
-  } 
-});
-wholeGrid.innerHTML = null;
-wholeGrid.appendChild(newGrid);
+  // Work around lazy loading of BC photos
+  setTimeout(() => {
+    Array.from(newGrid.querySelectorAll("img")).forEach((img) => {
+      if (img.dataset.original) {
+        // The actual src-URL lives on the data-original attribute
+        img.src = img.dataset.original;
+        // The lazy-loading sometimes gets caught halfway through a fade-in
+        // Zero-out the style in case any opacity is stuck anywhere below opacity:1.0
+        img.style = "";
+      }
+    });
+    // Finally, replace the merch grid with our cloned, modified elements
+    wholeGrid.innerHTML = null;
+    wholeGrid.appendChild(newGrid);
+  }, 1000);
+}
